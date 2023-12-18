c30cd04aeb23d90b235218d62c6e8dce40dacb1a
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2023-12-18 12:32:05 -0500

    Parse url with the params to get ToDos

--- src/services/environments/urls.js
+++ src/services/environments/urls.js
@@ -3,7 +3,18 @@ import { environment } from './environment'
 export const urls = {
   user: `${environment.apiUrl}/api/user`,
   auth: `${environment.apiUrl}/api/user/auth`,
-  todo: `${environment.apiUrl}/api/user/todo`,
-  getTodos: (userId, searhTerm, startDate, endDate) => `${environment.apiUrl}/api/user/todo?userId=${userId}&searhTerm=${searhTerm}&startDate=${startDate}&endDate=${endDate}`,
-  deleteTodo: todoId => `${environment.apiUrl}/api/user/todo/${todoId}`
+  todo: `${environment.apiUrl}/api/todo`,
+  getTodos: ({ filter }) => `${environment.apiUrl}/api/todo${filterToUrl({ filter })}`,
+  deleteTodo: todoId => `${environment.apiUrl}/api/todo/${todoId}`
+}
+
+const filterToUrl = ({ filter }) => {
+  let url = ''
+
+  if (filter?.userId) { url += `?userId=${filter.userId}` }
+  if (filter?.startDate) { url += `&startDate=${filter.startDate}` }
+  if (filter?.endDate) { url += `&endDate=${filter.endDate}` }
+  if (filter?.searchTerm) { url += `&searhTerm=${filter.searchTerm}` }
+
+  return url
 }
