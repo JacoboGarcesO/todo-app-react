c277a02a180831f34df7d40d772921e1b7eefe31
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2023-12-18 12:34:37 -0500

    Create ToDo component

--- src/view/components/Todo/Todo.jsx
+++ src/view/components/Todo/Todo.jsx
@@ -1,17 +1,37 @@
+import { useContext } from 'react'
+import { setTodoToUpdate, toggleForm } from '../../../state/todo/actions'
 import { EditIcon } from '../Icons/EditIcon'
 import { TrashIcon } from '../Icons/TrashIcon'
 import './Todo.css'
+import { AppContext } from '../../../state/AppContext'
 
 export const Todo = ({ todo }) => {
+  const { dispatch } = useContext(AppContext)
+
+  const formatDate = date => {
+    return new Date(date).toDateString()
+  }
+
+  const todoClasses = todo.isCompleted ? 'todo todo--is-completed' : 'todo'
+
+  const saveUpdateToUpdate = () => {
+    dispatch(setTodoToUpdate({ todo }))
+    dispatch(toggleForm())
+  }
+
+  // const deleteTodo = () => {
+
+  // }
+
   return (
-    <tr>
-      <td><input type='checkbox' name='isCompleted' id='isCompleted' /></td>
+    <tr className={todoClasses}>
+      <td><input type='checkbox' name='isCompleted' id='isCompleted' defaultChecked={todo.isCompleted} /></td>
       <td>{todo.name}</td>
       <td>{todo.description}</td>
-      <td>{todo.finishDate}</td>
+      <td>{formatDate(todo.finishDate)}</td>
       <td>
         <div className='todo__actions'>
-          <EditIcon size={24} />
+          <EditIcon size={24} callback={saveUpdateToUpdate} />
           <TrashIcon size={24} />
         </div>
       </td>
