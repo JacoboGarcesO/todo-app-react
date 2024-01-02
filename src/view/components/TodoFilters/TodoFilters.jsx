696fbd9b4b5ec9a227730767e3fcd785dd87efae
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2024-01-01 20:04:29 -0500

    Implement filters to find ToDos

--- src/view/components/TodoFilters/TodoFilters.jsx
+++ src/view/components/TodoFilters/TodoFilters.jsx
@@ -14,6 +14,10 @@ export const TodoFilters = () => {
     endDate: null
   })
 
+  const handleFilterChange = ({ target }) => {
+    setFilter(prevFilter => ({ ...prevFilter, [target.name]: target.value ?? null }))
+  }
+
   useEffect(() => {
     get({ filter })
       .then((todos) => {
@@ -25,16 +29,16 @@ export const TodoFilters = () => {
     <section className='todo-filters'>
       <form className='todo-filters__form'>
         <div className='todo-filters__input'>
-          <label htmlFor='name'>Name</label>
-          <input type='text' placeholder='Search...' id='name' />
+          <label htmlFor='searchTerm'>Name</label>
+          <input type='text' placeholder='Search...' id='searchTerm' name='searchTerm' onChange={handleFilterChange} />
         </div>
         <div className='todo-filters__input'>
           <label htmlFor='start-date'>Start date</label>
-          <input type='date' id='start-date' />
+          <input type='date' id='start-date' name='startDate' onChange={handleFilterChange} />
         </div>
         <div className='todo-filters__input'>
           <label htmlFor='finish-date'>Finish date</label>
-          <input type='date' id='finish-date' />
+          <input type='date' id='finish-date' name='endDate' onChange={handleFilterChange} />
         </div>
       </form>
       <Button type='button' text='New' callback={() => dispatch({ type: todoActions.TOGGLE_FORM })} />
